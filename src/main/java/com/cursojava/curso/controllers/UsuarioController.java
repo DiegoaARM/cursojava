eba18e380901e967f28bac0d10c93911b2c7bd5e
DiegoaARM <juan.duque31@udea.edu.co>
2023-09-25 18:58:01 -0500

    Nuevo proyecto

---
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -0,0 +1,44 @@
+package com.cursojava.curso.controllers;
+
+import com.cursojava.curso.dao.UsuarioDao;
+import com.cursojava.curso.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios() {
+        return usuarioDao.getUsuarios();
+    }
+
+    @RequestMapping(value = "usuario1")
+    public Usuario editar() {
+        Usuario usuario = new Usuario();
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario2")
+    public Usuario eliminar() {
+        Usuario usuario = new Usuario();
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario3")
+    public Usuario buscar() {
+        Usuario usuario = new Usuario();
+        return usuario;
+    }
+
+
+}
